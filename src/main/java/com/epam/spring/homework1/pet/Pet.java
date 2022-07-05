0b87859cd8d657d1d22cc2dcec2adf9a58cbc28e
ostap <ostap.semotiuk@gmail.com>
2022-07-05 20:08:48 +0300

    homework 1

---
+++ src/main/java/com/epam/spring/homework1/pet/Pet.java
@@ -0,0 +1,23 @@
+package com.epam.spring.homework1.pet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class Pet implements Animal {
+
+    private List<Animal> animals;
+
+    @Autowired
+    public Pet(List<Animal> animals){
+        this.animals = animals;
+    }
+
+    public void printPets(){
+        for(Animal e: animals)
+            System.out.println(e.getClass().getSimpleName() );
+    }
+
+}
