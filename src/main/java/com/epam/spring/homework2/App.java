6d2cd2f605eee371a45d81e62f83ec48c2e7dd62
ostap <ostap.semotiuk@gmail.com>
2022-07-12 12:16:27 +0300

    context closed

--- src/main/java/com/epam/spring/homework2/App.java
+++ src/main/java/com/epam/spring/homework2/App.java
@@ -2,6 +2,7 @@ package com.epam.spring.homework2;
 
 import com.epam.spring.homework2.config.Config;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import java.util.Arrays;
 
@@ -9,6 +10,6 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
         System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
-
+        ((ConfigurableApplicationContext)context).close();
     }
 }
